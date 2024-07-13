d4d90767c714a6f56f007a751ebd6a8a59cde18c
jessandria <jessie.andriamanankasina@gmail.com>
2024-07-13 07:56:00 +0400

    app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
     <Provider store={store}>
     <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
